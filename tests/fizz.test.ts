1f86fe3f2e87cb1c6ea091d3ad4fac41a558517d
ewen-r <ewen.reynolds@gmail.com>
2023-09-01 08:43:26 +0100

    More tests.

--- tests/fizz.test.ts
+++ tests/fizz.test.ts
@@ -15,8 +15,8 @@ describe("FizzBuzz",
       expect(fizzBuzz([15])).toBe('fizzbuzz');
     });
 
-    test('[1,2,3] should result in "1, 2, fizz"', () => {
-      expect(fizzBuzz([3])).toBe('fizz');
+    test('[1,2,3,5,11,15,29,30,32,45] should result in "1, 2, fizz, buzz, 11, fizzbuzz, 29, fizzbuzz, 32, fizzbuzz"', () => {
+      expect(fizzBuzz([1,2,3,5,11,15,29,30,32,45])).toBe('1, 2, fizz, buzz, 11, fizzbuzz, 29, fizzbuzz, 32, fizzbuzz');
     });
 
   }
--- tests/reverse.test.ts
+++ tests/reverse.test.ts
@@ -1,4 +1,4 @@
-import { reverse, reverse2 } from '../src/index';
+import { reverse, reverse1, reverse2, reverse3 } from '../src/index';
 
 
 describe("reverse",
@@ -26,6 +26,31 @@ describe("reverse",
 );
 
 
+describe("reverse1",
+  () => {
+    test('reverse1("fizz")', () => {
+      expect(reverse1('fizz')).toBe('zzif');
+    });
+
+    test('reverse1("feeb")', () => {
+      expect(reverse1('feeb')).toBe('beef');
+    });
+
+    test('reverse1("")', () => {
+      expect(reverse1('')).toBe('');
+    });
+
+    test('reverse1("12345")', () => {
+      expect(reverse1('12345')).toBe('54321');
+    });
+
+    test('reverse1("  ")', () => {
+      expect(reverse1('  ')).toBe('  ');
+    });
+  }
+);
+
+
 describe("reverse2",
   () => {
     test('reverse2("fizz")', () => {
@@ -46,6 +71,31 @@ describe("reverse2",
 
     test('reverse2("  ")', () => {
       expect(reverse2('  ')).toBe('  ');
+    });
+  }
+);
+
+
+describe("reverse3",
+  () => {
+    test('reverse3("fizz")', () => {
+      expect(reverse3('fizz')).toBe('zzif');
+    });
+
+    test('reverse3("feeb")', () => {
+      expect(reverse3('feeb')).toBe('beef');
+    });
+
+    test('reverse3("")', () => {
+      expect(reverse3('')).toBe('');
+    });
+
+    test('reverse3("12345")', () => {
+      expect(reverse3('12345')).toBe('54321');
+    });
+
+    test('reverse3("  ")', () => {
+      expect(reverse3('  ')).toBe('  ');
     });
   }
 );
